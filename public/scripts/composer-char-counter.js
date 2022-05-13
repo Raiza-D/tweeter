9fbce7d061d7db3197d8d1966c6eadf90531fc6b
Raiza De Guzman <santos_dg7@hotmail.com>
2022-05-13 16:24:05 -0700

    Add jQuery document ready code. This ensures the DOM loads first before we try to access any HTML elements. Added a console.log within the document ready callback in order to test it.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,1 +1,6 @@
 /* This file will be responsible for the character counter feature of our SPA */
+
+$(document).ready(function() {
+  // code goes here
+  console.log("Hello, testing this file!");
+});
