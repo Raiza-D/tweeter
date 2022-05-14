221b31c57dd326a68e4e836638d46495f21d0add
Raiza De Guzman <santos_dg7@hotmail.com>
2022-05-14 13:48:54 -0700

    During mentor session with Gary Jipp to seek help with changing counter color to red if char count below zero, we ended up re-writing my entire code. Now everything is encapsulated and uses IIFE (immediately invoke function expression). Mentor talked about other topics outside of changing the counter color and that's why my code is written the way it is. Also added some notes. May revert code back to how I had it before. The reason my if statement for changing counter color wasn't working was becuase it was outside of my function

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,33 @@
 /* This file will be responsible for the character counter feature of our SPA */
 
-$(document).ready(function() {
-  // code goes here
-  let maxLength = 140;
+(function($) {
 
-  $("textarea").keyup(function() {
-    let length = $(this).val().length;
-    length = maxLength - length;
-    $(".counter").text(length);
-  })
-});
+  $(document).ready(function() {
+    // code goes here
+
+    $("textarea").on("input", onChange);
+  });
+
+  const onChange = function() {
+    const maxLength = 10;
+
+    const form = $(this).closest("form");
+    const counter = form.find(".counter");
+
+    let inputLength = $(this).val().length;
+    const remainingLength = maxLength - inputLength;
+    counter.text(remainingLength);
+
+    if (remainingLength < 0) {
+      counter.addClass("red");
+      return;
+    }
+
+    counter.removeClass("red");
+  };
+
+})(jQuery);
+
+/* IIFE used if a function is only going to be used ONCE and we want to hide everything from the
+global scope so the variable names, etc are not interferred with.
+IIFE is to also used for encapsulation. */
