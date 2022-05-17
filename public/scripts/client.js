8dbef45d64dcf5949bb25895b5799a7c6e9ef205
Raiza De Guzman <santos_dg7@hotmail.com>
2022-05-17 16:04:01 -0700

    Corrected Line 7. Syntax error. Added 'document' in parentheses and made sure there was $ in front of it to indicate use of jQuery. createTweetElement function was not working when my browser page refreshed.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,7 +4,7 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-$document.ready(function() {
+$(document).ready(function() {
 
 const tweetData = {
     "user": {
