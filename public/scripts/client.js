a26d1c342756306e9af1f37058a40c00892c86c7
Raiza De Guzman <santos_dg7@hotmail.com>
2022-05-17 20:05:41 -0700

    1)Copy/pasted data array from Compass. The info inside will be used for both our createTweetElement function and the renderTweets function. 2)Define and implement renderTweets function, following hints on Compass. Function loops through each element in data array (which are objects). For each object in the iteration, invoke the createTweetElement which takes in that object as a parameter. Result is saved in  variable. Then taking the element with tweets-container id, we are going to append or ADD/GLUE the value stored in  variable. That way the tweets in the data object show on our page. 3)Console logged old code from first part of assignment.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,57 +5,69 @@
  */
 
 $(document).ready(function() {
-
-const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
+  const data = [
+    {
+      user: {
+        name: "Newton",
+        avatars: "https://i.imgur.com/73hZDYK.png",
+        handle: "@SirIsaac",
       },
-    "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
+      content: {
+        text: "If I have seen further it is by standing on the shoulders of giants",
       },
-    "created_at": 1461116232227
- }
+      created_at: 1652654002002,
+    },
+    {
+      user: {
+        name: "Descartes",
+        avatars: "https://i.imgur.com/nlhLi3I.png",
+        handle: "@rd",
+      },
+      content: {
+        text: "Je pense , donc je suis",
+      },
+      created_at: 1652740402002,
+    },
+  ];
 
+  const createTweetElement = function (tweet) {
+    let $tweet = `<article class="tweet-container">
+      <header class="inner-header">
+        <img src=${tweet.user.avatars}>
+        <div class="name-handle">
+          <span>${tweet.user.name}</span>
+          <a href="#">${tweet.user.handle}</a>
+        </div>
+      </header>
 
-const createTweetElement = function(tweet) {
-  let $tweet =
-  `<article class="tweet-container">
-    <header class="inner-header">
-      <img src=${tweet.user.avatars}>
-      <div class="name-handle">
-        <span>${tweet.user.name}</span>
-        <a href="#">${tweet.user.handle}</a>
-      </div>
-    </header>
+      <section class="article-body">
+        <p>${tweet.content.text}</p>
+      </section>
+      
+      <footer class="article-footer">
+        <span>${tweet.created_at}</span>
+        <div>
+        <i class="fa-solid fa-flag"></i>
+        <i class="fa-solid fa-retweet"></i>
+        <i class="fa-solid fa-heart"></i>
+        </div>
+      </footer>
+    </article>`;
 
-    <section class="article-body">
-      <p>${tweet.content.text}</p>
-    </section>
-    
-    <footer class="article-footer">
-      <span>${tweet.created_at}</span>
-      <div>
-      <i class="fa-solid fa-flag"></i>
-      <i class="fa-solid fa-retweet"></i>
-      <i class="fa-solid fa-heart"></i>
-      </div>
-    </footer>
-  </article>`;
+    return $tweet;
+  };
 
-  return $tweet;
-};
+  const renderTweets = function(tweets) {
+    for (const tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $("#tweets-container").append($tweet);
+    }
+    return tweets;
+  };
 
-// const $tweet = createTweetElement(tweetData);
-
-const renderTweets = function(tweets) {
-
-};
-
-
-console.log($tweet); // to see what it looks like
-$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-
+  renderTweets(data);
+  
+  //console.log($tweet); // to see what it looks like
+  // const $tweet = createTweetElement(tweetData);
+  // $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
 });
-
